a38a1804b58fa2a478c03031cd1a6890abd29c92
Max Timberhill <max.lisogorski@gmail.com>
2019-11-23 16:39:24 +0000

    new colours for plots, added example plots

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # radiant
-RADIAl velocity from liNe profiles simulaTor
+`RADIAl velocity from liNe profiles simulaTor`
+##### _(wild acronym by [ACRONYM](https://github.com/bacook17/acronym))_
+
+
+## Dependencies
+
+- Python 3
+- numpy
+- scipy
+- PyAstronomy
+- matplotlib
+- progressbar _(optional)_
+
+
+## Usage
+
+Set the values in `input.config`, such as the star's mass and rotation period, planet's mass and orbital parameters, input files with the line profiles, plotting and output paths and settings. The example is provided and should be used as a template, comments start with `#` symbol.
+
+
+## Example
+
+Using the provided `input.config` example:
+
+![Simulated observations](example-rv.png)
+![Lomb-Scargle periodogram](example-ls.png)
--- modules/rvplots.py
+++ modules/rvplots.py
@@ -8,7 +8,7 @@ plt.rc('font', family='serif')
 settings = Settings()
 
 def savePeriodogramPlot(freqs, periods, powers, P, fit_p, subfolder=''):
-	plt.figure(figsize=(15,5))
+	plt.figure(figsize=(10,3))
 	xvals = freqs
 	plt.xlabel(r'frequency, day$^{-1}$')
 
@@ -25,14 +25,13 @@ def savePeriodogramPlot(freqs, periods, powers, P, fit_p, subfolder=''):
 		plt.xscale('log')
 		plt.xlabel(r'log period, days')
 
-
 	ls_axis = [min(xvals), max(xvals), min(powers), max(powers)]
 	ls_axis[3] += (ls_axis[3] - ls_axis[2]) / 10.0
 	plt.axis(ls_axis)
-	plt.plot(xvals, powers, 'b-')
-	plt.plot((P, P), (0, max(powers)), 'b--', label='original period, P = ' + str(truncate(P)))
+	plt.plot(xvals, powers, color="#021c1e", linewidth=1)
+	plt.plot((P, P), (0, max(powers)), label='original period, P = ' + str(truncate(P)), color="#2c7873", linestyle="--")
 	if fit_p > 0:
-		plt.plot((fit_p, fit_p), (0, max(powers)), 'r-', label='fitted period, P = ' + str(truncate(fit_p)))
+		plt.plot((fit_p, fit_p), (0, max(powers)), label='fitted period, P = ' + str(truncate(fit_p)), color="#fb6542", linestyle="-")
 	
 	plt.ylabel(r'power')
 	plt.legend(loc='best')
@@ -45,6 +44,7 @@ def savePeriodogramPlot(freqs, periods, powers, P, fit_p, subfolder=''):
 	else:
 		plt.clf()
 
+
 def saveRVPlot(ts, ts_full, rvs, rv_full_fit, rv_full_orig, rv_fit, rv_orig, Ms, SNR, params, params_fit, params_err, subfolder=''):
 
 	# params = Mp, p, a, e, w, v0
@@ -55,7 +55,7 @@ def saveRVPlot(ts, ts_full, rvs, rv_full_fit, rv_full_orig, rv_fit, rv_orig, Ms,
 	w = [params[4], params_fit[4], params_err[4]]
 	v0 = [params[5], params_fit[5], params_err[5]]
 
-	plt.figure(figsize=(15,12))
+	plt.figure(figsize=(10,5))
 	plt.suptitle(r'$M_\bigstar = ' + str(truncate(Ms)) + ' M_\odot$, $SNR=' + str(SNR) + '$\n' + \
 		'Original orbit: $M_p = ' + str(truncate(Mplanet[0])) + ' M_\oplus$, $P = ' + str(truncate(P[0])) + '$ days, $a = ' + str(truncate(a[0])) + \
 		'$ AU, $e = ' + str(truncate(e[0])) + '$, $w = '+ str(truncate(w[0])) + '$ rad, $v_0 = '+ str(truncate(v0[0])) + '$ m/s \n' + \
@@ -63,28 +63,28 @@ def saveRVPlot(ts, ts_full, rvs, rv_full_fit, rv_full_orig, rv_fit, rv_orig, Ms,
 			+ str(truncate(P[1])) + '\pm' + str(truncate(P[2])) + '$ days, $a = ' + str(truncate(a[1])) + '\pm'+ str(truncate(a[2])) + \
 		'$ AU, $e = ' + str(truncate(e[1])) + '\pm' + str(truncate(e[2])) + '$, $w = '+ str(truncate(w[1])) + '\pm' + str(truncate(w[2])) + \
 			'$ rad, $v_0 = '+ str(truncate(v0[1])) + '\pm'+ str(truncate(v0[2])) + '$ m/s',\
-		fontsize=16)
+		fontsize=10)
 	gs = gridspec.GridSpec(3, 1, height_ratios=[4, 1, 1])
 	gs.update(hspace=0.05)
 	ax0 = plt.subplot(gs[0])
 	ax1 = plt.subplot(gs[1], sharex=ax0)
 	ax2 = plt.subplot(gs[2], sharex=ax0)
 
-	ax0.plot(ts_full, rv_full_orig, 'b--', label='original curve')
-	ax0.plot(ts, rvs, 'bo', label='datapoints', alpha=0.5)
-	ax0.plot(ts_full, rv_full_fit, 'r-', label='fit result')
+	ax0.plot(ts_full, rv_full_orig, label='original curve', color="#2c7873", linestyle="--")
+	ax0.plot(ts, rvs, label='datapoints', alpha=0.7, marker="o", markerfacecolor="#021c1e", markeredgecolor="none", linestyle="none")
+	ax0.plot(ts_full, rv_full_fit, label='fit result', color="#fb6542")
 	ax0.legend(loc=1)
 
 	tmin = min(ts)
 	tmax = max(ts)
 
 	# residuals
-	ax1.plot((tmin, tmax), (0, 0), 'b--', label='original curve')
-	ax1.plot(ts_full, rv_full_fit - rv_full_orig, 'r-', label='fit')
-	ax1.plot(ts, rvs - rv_orig, 'bo', label='data points', alpha=0.5)
+	ax1.plot((tmin, tmax), (0, 0), label='original curve', color="#2c7873", linestyle="--")
+	ax1.plot(ts_full, rv_full_fit - rv_full_orig, label='fit', color="#fb6542")
+	ax1.plot(ts, rvs - rv_orig, label='data points', alpha=0.7, marker="o", markerfacecolor="#021c1e", markeredgecolor="none", linestyle="none")
 
-	ax2.plot((tmin, tmax), (0, 0), 'r-', label='fit')
-	ax2.plot(ts, rvs - rv_fit, 'bo', label='data points', alpha=0.5)
+	ax2.plot((tmin, tmax), (0, 0), label='fit', color="#fb6542")
+	ax2.plot(ts, rvs - rv_fit, label='data points', alpha=0.7, marker="o", markerfacecolor="#021c1e", markeredgecolor="none", linestyle="none")
 
 	ax0.set_ylabel(r'RV, m/s')
 	ax2.set_xlabel(r'time, days')
